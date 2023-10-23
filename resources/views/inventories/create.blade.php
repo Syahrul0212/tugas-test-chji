70c3ff4531311efc2bacd390ae99dafc69fab261
Syahrul02 <syhrl44@gmail.com>
2023-10-23 22:38:25 +0700

    update

--- resources/views/inventories/create.blade.php
+++ resources/views/inventories/create.blade.php
@@ -5,19 +5,19 @@
 
     <form action="{{ route('inventories.store') }}" method="POST">
         @csrf
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="nama_barang">Name:</label>
             <input type="text" class="form-control" id="nama_barang" name="nama_barang" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="jumlah">Quantity:</label>
             <input type="number" class="form-control" id="jumlah" name="jumlah" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="tanggal_masuk">Tanggal Masuk:</label>
             <input type="date" class="form-control" id="tanggal_masuk" name="tanggal_masuk" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="tanggal_keluar">Tanggal Keluar:</label>
             <input type="date" class="form-control" id="tanggal_keluar" name="tanggal_keluar" required>
         </div>
--- resources/views/inventories/edit.blade.php
+++ resources/views/inventories/edit.blade.php
@@ -6,19 +6,19 @@
     <form action="{{ route('inventories.update', $inventory->id) }}" method="POST">
         @csrf
         @method('PUT')
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="nama_barang">Name:</label>
             <input type="text" class="form-control" id="nama_barang" name="nama_barang" value="{{ $inventory->nama_barang }}" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="jumlah">Quantity:</label>
             <input type="number" class="form-control" id="jumlah" name="jumlah" value="{{ $inventory->jumlah }}" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="tanggal_masuk">Tanggal Masuk:</label>
             <input type="date" class="form-control" id="tanggal_masuk" name="tanggal_masuk" required>
         </div>
-        <div class="form-group">
+        <div class="form-group mb-3">
             <label for="tanggal_keluar">Tanggal Keluar:</label>
             <input type="date" class="form-control" id="tanggal_keluar" name="tanggal_keluar" required>
         </div>
